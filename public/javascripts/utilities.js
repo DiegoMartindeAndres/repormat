3b4b39f6a8527b95312031487403f1347f61ca70
Juan I Rodriguez <juanrodriguezliebana@gmail.com>
2019-06-08 20:45:15 +0200

    Añadido acceso desde previsualizacion a la descarga en una ventana nueva. Pinchar en documento hace aparecer un boton con el enlace.

---
+++ public/javascripts/utilities.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  //$('.modal.print').addClass('visible');
+  $('body').on('click',()=>{$('.modal.print').toggleClass('visible');})
+  $('.moda.print').on('click',()=>{$('.modal.print').toggleClass('visible');})
+
+
+
+});
--- routes/print.js
+++ routes/print.js
@@ -9,16 +9,24 @@ router.get('/', function(req, res, next){
   }).then(browser => {
     browser.newPage().then(async page => {
       await page.goto('http://localhost:8080/', {waitUntil: 'networkidle0'}); //Navigate to template endpoint
-      //await page.emulateMedia('screen');
+      await page.addStyleTag({
+        content: "body .page {box-shadow: none;margin: 0; }"
+      })
+      await page.emulateMedia('screen');
       console.log("Creando pdf...");
       await page.pdf({
-        path: 'prueba.pdf',
+        //path: 'prueba.pdf',
         format: 'A4'
+        //width: '21cm',
+        //height: '29.71cm'
+      }).then(pdf => {
+        res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length });
+	      res.send(pdf);
       });
       browser.close();
     });
   });
-   res.redirect('/');
+   //res.redirect('/');
 });
 
 module.exports = router;
